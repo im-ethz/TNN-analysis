4792bf5f25a6c5de9cd5c7dc1f60d9f4fea36fff
evanweenen <evanweenen@ethz.ch>
2020-11-25 18:54:13 +0100

    Update trainingpeaks scraper

--- Code/scrape_trainingpeaks.py
+++ Code/scrape_trainingpeaks.py
@@ -7,6 +7,10 @@ from config import username_TP, password_TP, username_LV, password_LV
 import pyderman as dr
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+import time
 
 download_path = '/local/home/evanweenen/bicyclediabetes/Code/Data/'
 
@@ -27,59 +31,62 @@ driver.get(url_login)
 driver.find_element_by_name('Username').send_keys(username_TP)
 driver.find_element_by_name('Password').send_keys(password_TP)
 driver.find_element_by_name('submit').click()
-driver.implicitly_wait(10)
+driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
 
 # select athlete
 driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[2]/div/div/button").click() # dropdown menu
-driver.implicitly_wait(2)
+driver.set_page_load_timeout(10)#driver.implicitly_wait(2)
 athlete_elements = driver.find_elements_by_class_name('athleteOption') # list of athletes
 
 for i in range(1, len(athlete_elements)):
 	athlete_elements[i].click() # click on ith athlete
-	driver.implicitly_wait(3)
+	time.sleep(5)#driver.set_page_load_timeout(10)#driver.implicitly_wait(20)
 	
 	# go to list layout
 	driver.find_element_by_class_name('searchButton').click()
-	driver.implicitly_wait(3)
-
-	driver.find_element_by_class_name('filter').click()
-	driver.implicitly_wait(5)
+	time.sleep(2)#driver.set_page_load_timeout(10)#driver.implicitly_wait(3)
 
 	# select only bike training (only first time visiting website)
 	if i == 1:
+		driver.find_element_by_class_name('filter').click()
+		driver.set_page_load_timeout(10)#driver.implicitly_wait(20)
+
 		driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[3]/div[3]/div/div[2]/div[5]/div[4]/div[2]/label[2]").click() # select bike
-		driver.implicitly_wait(10)
+		driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
 		#driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[3]/div[3]/div/div[2]/div[5]/div[5]/div/div[2]/label[1]").click() # select cycling
 
 
 	for d in range(len(start_dates_list)):
 		driver.find_element_by_class_name('startDate').clear()
 		driver.find_element_by_class_name('startDate').send_keys(start_dates_list[d]+'\n')
+		time.sleep(1)
 
 		driver.find_element_by_class_name('endDate').clear()
 		driver.find_element_by_class_name('endDate').send_keys(end_dates_list[d]+'\n')
-		driver.implicitly_wait(5)
+		time.sleep(5)#driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
 
 		activities_elements = driver.find_elements_by_class_name("activity")
 		for j in range(int(driver.find_element_by_class_name('totalHits').text.strip(' results'))):
 			
 			driver.find_elements_by_class_name("activity")[j].click()
-			driver.implicitly_wait(4)
+			time.sleep(1)#driver.set_page_load_timeout(10)driver.implicitly_wait(20)
 
-			driver.find_element_by_id('quickViewFileUploadDiv').click()
-			driver.implicitly_wait(4)
+			if driver.find_element_by_id('quickViewFileUploadDiv').text != 'Upload':
+				driver.find_element_by_id('quickViewFileUploadDiv').click()
+				driver.set_page_load_timeout(10)#driver.implicitly_wait(4)
 
-			# download
-			driver.find_element_by_class_name('download').click()
-			driver.implicitly_wait(2)
+				# download
+				driver.find_element_by_class_name('download').click()
+				driver.set_page_load_timeout(10)#driver.implicitly_wait(2)
 
 			driver.find_element_by_id('closeIcon').click()
-			driver.implicitly_wait(2)
+			driver.set_page_load_timeout(10)#driver.implicitly_wait(2)
 
 	driver.find_element_by_class_name('closeIcon').click()
+	driver.set_page_load_timeout(10)
 
 	driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[2]/div/div/button").click() # dropdown menu
-	driver.implicitly_wait(1)
+	driver.set_page_load_timeout(10)#driver.implicitly_wait(1)
 	athlete_elements = driver.find_elements_by_class_name('athleteOption') # list of athletes
 
 
