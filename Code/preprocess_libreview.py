9c4697d70f1702a9d0a1bf65e0ec2b0e74b345bf
evanweenen <evanweenen@ethz.ch>
2020-12-07 20:04:29 +0100

    Figure out what record types are + unstack new rows for each record type in timestamp

--- Code/preprocess_libreview.py
+++ Code/preprocess_libreview.py
@@ -19,7 +19,7 @@ for i in files:
 	df = pd.read_csv(path+'raw_anonymous/'+str(i)+'.csv', skiprows=2)
 
 	if df.empty:
-		print(i, " is empty")
+		print("WARNING: LibreView data for Athlete ", i, " is empty")
 		continue
 
 	# drop duplicates #TODO: why are there duplicates in the data??
@@ -35,23 +35,49 @@ for i in files:
 	
 	# check for items with same timestamp but different glucose levels
 	df_dupl = df[df.duplicated(subset=['Device Timestamp', 'Record Type', 'Device', 'Serial Number'], keep=False)]
-	print("Duplicates ", df_dupl.drop(['Device', 'Serial Number'], axis=1).dropna(axis=1, how='all'))
+	if not df_dupl.empty:
+		print("WARNING: Items with same timestamp but different glucose levels\n",
+			df_dupl.drop(['Device', 'Serial Number'], axis=1).dropna(axis=1, how='all'))
 	timestamp_dupl = df_dupl['Device Timestamp'].unique()
 
+	# for the items with same timestamp but different glucose levels:
 	# for record type 0 or 1, keep the last item, because that's probably the right item #TODO
 	df[df['Record Type'] <= 1] = df[df['Record Type'] <= 1].drop_duplicates(subset=['Device Timestamp', 'Record Type', 'Device', 'Serial Number'], keep='last', ignore_index=True)
-	df.dropna(how='all', inplace=True)
+	df.dropna(how='all', inplace=True) #this is necessary because previous step sets rows to nan somehow
 
+	# prints items with the same timestamp but different glucose levels that did not get removed
+	# (because they don't have record type 0 or 1)
 	df_dupl = df[df.duplicated(subset=['Device Timestamp', 'Record Type', 'Device', 'Serial Number'], keep=False)]
 	if not df_dupl.empty:
-		print("Still duplicates", df_dupl.drop(['Device', 'Serial Number'], axis=1).dropna(axis=1, how='all'))
+		print("WARNING: After fixing the items with record type 0 or 1, there are still items with the same timestamp but different glucose levels\n", 
+			df_dupl.drop(['Device', 'Serial Number'], axis=1).dropna(axis=1, how='all'))
 
 	#print("Dupl timestamp", df[(df['Device Timestamp'].isin(timestamp_dupl))].drop(['Device', 'Serial Number'], axis=1).dropna(axis=1, how='all'))
 
 	print("Empty columns: ", set(df.columns) - set(df.dropna(axis=1, how='all').columns))
-	print("Non-empty columns: ", df.dropna(axis=1, how='all').columns)
-
-	print("Notes: ", df['Notes'].dropna())
+	print("Non-empty columns: ", set(df.dropna(axis=1, how='all').columns))
 
 	# TODO: process notes
-	df.to_csv(path+'clean/'+str(i)+'.csv', index_label=False)
+	if not df['Notes'].dropna().empty:
+		print("TODO: The following notes are present in the file.\n", df['Notes'].dropna().values)
+
+	"""
+	# print columns associated with each record type
+	for j in range(int(df['Record Type'].max())+1):
+		print("Columns associated with Type %s : "%j, 
+			set(df[df['Record Type'] == j].dropna(axis=1, how='all').columns) 
+			- set(['Device', 'Serial Number', 'Device Timestamp', 'Record Type']))
+	"""
+
+	# create new dataframe without duplicate rows for each record type in a timestamp
+	df = df.groupby('Device Timestamp').first().drop('Record Type', axis=1)
+
+	"""
+	df.set_index('Device Timestamp', drop=True, inplace=True)
+	df_joinlist = {j : df[df['Record Type'] == j].dropna(axis=1, how='all').drop(['Device', 'Serial Number', 'Record Type'], axis=1) 
+		for j in range(int(df['Record Type'].max())+1) if not df[df['Record Type'] == j].empty}
+	df_joinstart = df[['Device', 'Serial Number']].reset_index().drop_duplicates().set_index('Device Timestamp', drop=True)
+	df_join = df_joinstart.join(df_joinlist.values(), how='outer')
+	"""
+
+	df.to_csv(path+'clean/'+str(i)+'.csv')
