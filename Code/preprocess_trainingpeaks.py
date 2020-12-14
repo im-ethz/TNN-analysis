4ca289aad32e1a82d2bfb6b3d51eaeac10c054a9
evanweenen <evanweenen@ethz.ch>
2020-12-14 17:05:13 +0100

    Move unknown columns to nan_file + create list of dates and files corresponding to that date + show number of duplicated entries, number of duplicated timestamps and days with duplicated timestamps + show days with duplicate files but not duplicate timestamps and vice versa

--- Code/preprocess_trainingpeaks.py
+++ Code/preprocess_trainingpeaks.py
@@ -21,6 +21,7 @@ for i in athletes:
 	filename_date = {}
 
 	df = pd.DataFrame()
+	df_nan = pd.DataFrame()
 
 	files = sorted(os.listdir(path+'csv/'+str(i)+'/data'))
 	for f in files:
@@ -45,6 +46,11 @@ for i in athletes:
 			df_data['local_timestamp'] = np.nan
 
 		# open all other files and check for missing info
+		try:
+			df_n = pd.read_csv(path+'csv/'+str(i)+'/nan/'+name+'_nan.csv', index_col=0)
+			df_nan = df_nan.append(df_n, ignore_index=True, verify_integrity=True)
+		except FileNotFoundError:
+			pass
 
 		df = df.append(df_data, ignore_index=True, verify_integrity=True)
 
@@ -74,6 +80,11 @@ for i in athletes:
 				df_data['local_timestamp'] = np.nan
 
 			# open all other files and check for missing info
+			try:
+				df_n = pd.read_csv(path+'csv/'+str(i)+'/Zwift/nan/'+name+'_nan.csv', index_col=0)
+				df_nan = df_nan.append(df_n, ignore_index=True, verify_integrity=True)
+			except FileNotFoundError:
+				pass
 
 			df = df.append(df_data, ignore_index=True, verify_integrity=True)
 
@@ -83,9 +94,31 @@ for i in athletes:
 	date_filename = pd.DataFrame.from_dict(filename_date, orient='index').stack().reset_index()\
 		.rename(columns={'level_0':'file', 0:'date'}).drop('level_1', axis=1)\
 		.groupby('date').apply(lambda x:np.unique(x))
+	date_filename = pd.DataFrame(date_filename, columns=['files'])
+	date_filename['N'] = date_filename['files'].apply(lambda x:len(x))
 	date_filename.to_csv(path+'clean/'+str(i)+'/'+str(i)+'_datefilename.csv')
 
+	# remove unknown columns from into nans file TODO: process later
+	cols_unk = df.columns[df.columns.str.startswith('unknown')]
+	df_nan = df_nan.append(df[['timestamp']+list(cols_unk)].dropna(subset=cols_unk, how='all'))
+	df.drop(cols_unk, axis=1, inplace=True)
 
+	# print duplicate timestamps # TODO
+	print("Number of duplicated entries: ", df.drop('Unnamed: 0', axis=1).duplicated().sum())
+	print("Number of duplicated timestamps: ", df['timestamp'].duplicated().sum())
+	print("Number of days with duplicate timestamps: ", len(df[df['timestamp'].duplicated()]['timestamp'].dt.date.unique()))
+
+	df_dupl = df[df['timestamp'].duplicated(keep=False)]
+	if not df_dupl.empty:
+		df_dupl['date'] = df_dupl['timestamp'].dt.date
+		df_dupl[df_dupl['timestamp'] == df_dupl.iloc[0]['timestamp']]
+
+		print("Dates with multiple files but not duplicate timestamps:\n",
+			*tuple(set(pd.to_datetime(date_filename[date_filename['N'] > 1].index).date.tolist()) 
+				- set(df_dupl['date'].unique())))
+		print("Dates with duplicate timestamps but not multiple files:\n",
+			*tuple(set(df_dupl['date'].unique()) 
+				- set(pd.to_datetime(date_filename[date_filename['N'] > 1].index).date.tolist())))
 
 	# save df to file
 	df.to_csv(path+'clean/'+str(i)+'/'+str(i)+'_data.csv', index_label=False)
