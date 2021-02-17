60e1fb4f27d89e18c1660ebb67663ff0504383fb
evanweenen <evanweenen@ethz.ch>
2021-02-17 16:12:20 +0100

    Remove separate preprocessing Zwift + update for 2019

--- Code/parse_fit_to_csv.sh
+++ Code/parse_fit_to_csv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# TODO: we don't need to distinguish anymore between Zwift and not-Zwift
-path='Data/TrainingPeaks/'
-rawPath='raw_anonymous/'
+path='Data/TrainingPeaks/2019/'
+path_raw='raw_anonymous/'
 athletes=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
 
 mkdir "${path}fit/"
@@ -11,22 +10,12 @@ do
 	mkdir "${path}fit/${i}"
 	mkdir "${path}csv/${i}"
 	echo "Processing athlete ${i}"
-	for filepath in "${path}${rawPath}${i}/"*.gz
+	for filepath in "${path}${path_raw}${i}/"*.gz
 	do
 		filename=$(basename "${filepath}" .fit.gz)
 		echo "${filename}"
 		gzip -dk "${filepath}"
-		mv "${path}${rawPath}${i}/${filename}.fit" "${path}fit/${i}/${filename}.fit"
+		mv "${path}${path_raw}${i}/${filename}.fit" "${path}fit/${i}/${filename}.fit"
 		python3 parse_fit_to_csv.py "${filename}.fit" -i "${path}fit/${i}/" -o "${path}csv/${i}/"
 	done
-	mkdir "${path}fit/${i}/Zwift"
-	mkdir "${path}csv/${i}/Zwift"
-	for filepath in "${path}${rawPath}${i}/Zwift/"*.gz
-	do
-		filename=$(basename "${filepath}" .fit.gz)
-		echo "${filename}"
-		gzip -dk "${filepath}"
-		mv "${path}${rawPath}${i}/Zwift/${filename}.fit" "${path}fit/${i}/Zwift/${filename}.fit"
-		python3 parse_fit_to_csv.py "${filename}.fit" -i "${path}fit/${i}/Zwift/" -o "${path}csv/${i}/Zwift/"
-	done	
 done
