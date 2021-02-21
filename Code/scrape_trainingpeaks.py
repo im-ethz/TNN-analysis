4e4823f6001a3af92c445b0c9125b34a44ac90b4
evanweenen <evanweenen@ethz.ch>
2021-02-21 21:31:40 +0100

    Scraping 2019

--- Code/scrape_trainingpeaks.py
+++ Code/scrape_trainingpeaks.py
@@ -21,7 +21,7 @@ url_login = 'https://home.trainingpeaks.com/login'
 #start_dates_list = ['1/1/2020', '1/2/2020', '1/3/2020', '1/4/2020', '1/5/2020', '1/6/2020', '1/7/2020', '1/8/2020', '1/9/2020', '1/10/2020']
 start_dates_list = ['1/1/2019', '1/2/2019', '1/3/2019', '1/4/2019', '1/5/2019', '1/6/2019', '1/7/2019', '1/8/2019', '1/9/2019', '1/10/2019', '1/11/2019', '1/12/2019']
 #end_dates_list = ['31/1/2020', '29/2/2020', '31/3/2020', '30/4/2020', '31/5/2020', '30/6/2020', '31/7/2020', '31/8/2020', '30/9/2020', '31/10/2020']
-end_dates_list = ['31/1/2019', '28/2/2019', '31/3/2019', '30/4/2019', '31/5/2019', '30/6/2019', '31/7/2019', '31/8/2019', '30/9/2019', '31/10/2020', '30/11/2019', '31/12/2019']
+end_dates_list = ['31/1/2019', '28/2/2019', '31/3/2019', '30/4/2019', '31/5/2019', '30/6/2019', '31/7/2019', '31/8/2019', '30/9/2019', '31/10/2019', '30/11/2019', '31/12/2019']
 
 path = dr.install(browser=dr.chrome, file_directory='./lib/', verbose=True, chmod=True, overwrite=False, version='86.0.4240.22', filename=None, return_info=False)
 options = webdriver.ChromeOptions()
@@ -35,11 +35,11 @@ driver.get(url_login)
 driver.find_element_by_name('Username').send_keys(username_TP)
 driver.find_element_by_name('Password').send_keys(password_TP)
 driver.find_element_by_name('submit').click()
-driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
+time.sleep(5)#driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
 
 # select athlete
 driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[2]/div/div/button").click() # dropdown menu
-driver.set_page_load_timeout(10)#driver.implicitly_wait(2)
+time.sleep(5)#driver.set_page_load_timeout(10)#driver.implicitly_wait(2)
 athlete_elements = driver.find_elements_by_class_name('athleteOption') # list of athletes
 
 for i in range(1, len(athlete_elements)):
@@ -58,7 +58,6 @@ for i in range(1, len(athlete_elements)):
 		driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[3]/div[3]/div/div[2]/div[5]/div[4]/div[2]/label[2]").click() # select bike
 		driver.set_page_load_timeout(10)#driver.implicitly_wait(10)
 		#driver.find_element_by_xpath("//*[@id='main']/div[1]/div/div/div[3]/div[3]/div/div[2]/div[5]/div[5]/div/div[2]/label[1]").click() # select cycling
-
 
 	for d in range(len(start_dates_list)):
 		driver.find_element_by_class_name('startDate').clear()
