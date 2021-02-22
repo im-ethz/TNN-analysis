b7ac70bafe89a3d20fa12e617ef4fd257b9d5cb0
evanweenen <evanweenen@ethz.ch>
2021-02-22 15:32:31 +0100

    Update helper with month to firstday mapping

--- Code/helper.py
+++ Code/helper.py
@@ -10,6 +10,8 @@ glucose_levels = {'hypo L2': (0,53),
 
 month_mapping = {1:'january', 2:'february', 3:'march', 4:'april', 5:'may', 6:'june', 7:'july',
 				 8:'august', 9:'september', 10:'october', 11:'november', 12:'december'}
+month_firstday = {'january':0, 'february':31, 'march':59, 'april':90, 'may':120, 'june':151, 'july':181,
+				'august':212, 'september':243, 'october':273, 'november':304, 'december':334}
 
 def create_calendar_array(df, datacol):
 	"""
