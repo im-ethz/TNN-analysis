97c15e3e6d2a6c27e7652ae1c9192de08d5e90ed
evanweenen <evanweenen@ethz.ch>
2021-02-22 10:05:07 +0100

    Update parsing for athletes with license

--- Code/parse_fit_to_csv.sh
+++ Code/parse_fit_to_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 path='Data/TrainingPeaks/2019/'
 path_raw='raw_anonymous/'
-athletes=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18")
+athletes=("1" "2" "3" "4" "5" "6" "10" "12" "13" "14" "15")
 
 mkdir "${path}fit/"
 mkdir "${path}csv/"
