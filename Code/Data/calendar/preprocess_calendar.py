025ce9efebe4e70db5159ff43462359964d1631a
evanweenen <evanweenen@ethz.ch>
2021-04-12 21:13:15 +0200

    Add calendar index

--- Code/Data/calendar/preprocess_calendar.py
+++ Code/Data/calendar/preprocess_calendar.py
@@ -20,11 +20,14 @@ print("RIDER nan: ", df[df.RIDER.isna()].dropna(axis=1))
 df.dropna(subset=['RIDER'], inplace=True)
 df['RIDER'] = df['RIDER'].map(rider_mapping)
 
-# reindex
-df = df.reset_index()
+# reindex, create training index and create calendar index
+df.reset_index(inplace=True)
 df.drop('level_0', axis=1, inplace=True)
 df.rename(columns={'level_1':'TrainingIndex'}, inplace=True)
 df.sort_values(['RIDER', 'TrainingIndex'], inplace=True)
-df.set_index(['RIDER', 'TrainingIndex'], inplace=True)
+df.reset_index(drop=True, inplace=True)
+df.reset_index(inplace=True)
+df.rename(columns={'index':'CalendarIndex'}, inplace=True)
+df.set_index(['RIDER', 'TrainingIndex'], drop=True, inplace=True)
 
 df.to_csv(path+'calendar_2019_anonymous.csv')
