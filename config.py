f9555d133dbeb28b02a8812087d5ef078a9d07b3
evanweenen <evanweenen@ethz.ch>
2022-01-20 11:53:01 +0100

    Save credentials and mapping in secret yml file

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-DATA_PATH = '/mnt/mtec-home/hype/data/'
+import yaml
+
+with open('credentials.yml', 'r') as f:
+    credentials = yaml.safe_load(f) 
+
+with open('mapping.yml', 'r') as f:
+    rider_mapping = yaml.safe_load(f)
+
+rider_mapping_inv = {v:k for k,v in rider_mapping.items()}
+
+DATA_PATH = '/wave/hypex/data/'
