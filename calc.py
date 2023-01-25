fc3dff1be9c7b082ed4cf618789522546288c553
evanweenen <evanweenen@ethz.ch>
2023-01-25 15:16:21 +0100

    Correctly handle nans in calculation of hypo and hyper

--- calc.py
+++ calc.py
@@ -57,6 +57,7 @@ def hypo(X:pd.Series, mintime:str='14min45S', unit:str='mgdl'):
         res = (X * mmoll_mgdl < glucose_levels['target'][0]).astype(int)
     elif unit == 'mgdl':
         res = (X < glucose_levels['target'][0]).astype(int)
+    res.loc[X.isna()] = np.nan
     res = minimum_eventtime(res, mintime)
     return res
 
@@ -69,6 +70,7 @@ def hyper(X:pd.Series, mintime:str='14min45S', unit:str='mgdl'):
         res = (X * mmoll_mgdl > glucose_levels['target'][1]).astype(int)
     elif unit == 'mgdl':
         res = (X > glucose_levels['target'][1]).astype(int)
+    res.loc[X.isna()] = np.nan
     res = minimum_eventtime(res, mintime)
     return res
 
